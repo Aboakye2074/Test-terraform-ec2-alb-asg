b2ed6ce4c5a6217d69ab4f7ffc7ae6273e194219
Aboakye2074 <70584084+Aboakye2074@users.noreply.github.com>
2024-07-15 13:07:30 -0400

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo yum update -y
-sudo yum install -y httpd
+sudo apt update -y
+sudo apt install -y httpd
 sudo systemctl start httpd
 sudo systemctl enable httpd
 echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
