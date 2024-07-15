07c4b3b9190693bd6649aa4a251f7b86fda4cccd
Aboakye2074 <70584084+Aboakye2074@users.noreply.github.com>
2024-07-15 13:05:10 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,8 +9,6 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region                   = "us-east-1"
-  shared_config_files      = ["/Users/sharmilas/.aws/config"]
-  shared_credentials_files = ["/Users/sharmilas/.aws/credentials"]
-  profile                  = "sharmi"
+  region                   = "us-east-2"
+  profile                  = "tony"
 }
